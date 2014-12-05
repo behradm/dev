31af0860d0e5fb8bbff4d708255cc1c6038d2c75
Behrad mirafshar <behrad.nakoja@gmail.com>
2014-12-05 18:06:40 +0100

    extra info in the footer dsables

--- wp-content/themes/b-afsh/footer.php
+++ wp-content/themes/b-afsh/footer.php
@@ -11,11 +11,14 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
+		<!--
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'b-afsh' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'b-afsh' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
 			<?php printf( __( 'Theme: %1$s by %2$s.', 'b-afsh' ), 'b-afsh', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
-		</div><!-- .site-info -->
+		</div>-->
+		<!-- .site-info -->
+
 	</footer><!-- #colophon -->
 </div><!-- #page -->
 
