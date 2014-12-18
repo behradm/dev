cb5a280d79f1b44ac0f2f6df67b481732f3b2c60
Behrad mirafshar <behrad.nakoja@gmail.com>
2014-12-18 15:50:06 +0100

    site-branding h1 tags disables

--- wp-content/themes/b-afsh/header.php
+++ wp-content/themes/b-afsh/header.php
@@ -22,11 +22,12 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'b-afsh' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
+		<!-- .site-branding
 		<div class="site-branding">
 			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-		</div><!-- .site-branding -->
-
+		</div>
+		-->
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle" aria-controls="menu" aria-expanded="false"><?php _e( 'Primary Menu', 'b-afsh' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
