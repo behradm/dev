2ff01c798ce2feb1aa4366d6816099a9037323a4
Behrad mirafshar <behrad.nakoja@gmail.com>
2015-03-11 18:04:41 +0100

    google analytics

--- wp-content/themes/b-afsh/header.php
+++ wp-content/themes/b-afsh/header.php
@@ -20,6 +20,7 @@
 </head>
 
 <body <?php body_class(); ?>>
+<?php include_once("analyticstracking.php") ?>
 <div id="page" class="hfeed site">
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'b-afsh' ); ?></a>
 
